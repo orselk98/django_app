fc2ea8cd1876b58d8adeb81768702091e61761c9
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2025-12-04 20:53:47 +0100

    New models for db, migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subject
+
+admin.site.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display =("id","name")
+    search_fields =("name",)
+
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
+class StudySession(models.Model):
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    date = models.DateField()
+    duration_minutes = models.IntegerField()
+    notes = models.TextField(blank = True)
+
+
 # Create your models here.
