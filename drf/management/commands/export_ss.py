740616651bda76ee5c815b8d0d42120d7b89936d
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-02-04 21:55:33 +0100

    Initialized Pandas + First export to CSV

--- drf/management/commands/export_ss.py
+++ drf/management/commands/export_ss.py
@@ -3,3 +3,33 @@ from core.models import Subject, StudySession
 from pathlib import Path
 import pandas as pd
 
+class Command(BaseCommand):
+    help="Export all Studysessions to a CSV file"
+
+    def handle(self,*args, **options):
+        queryset=StudySession.objects.all().values(
+            "id",
+            "datetime",
+            "duration_minutes",
+            "notes",
+        )
+        if not queryset.exists():
+            self.stdout.write(self.style.WARNING("No StudySessions found"))
+            return
+        
+        #Convert queryset to Dataframe
+        df=pd.DataFrame.from_records(queryset)
+
+        #Output file
+        output_dir=Path("exports")
+        output_dir.mkdir(exist_ok=True)
+
+        output_file=output_dir/ "study_sessions.csv"
+
+        df.to_csv(output_file,index=False)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Exported {len(df)} StudySessions to {output_file}"
+            )
+        )
